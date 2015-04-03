162ef0bc62f3813bcaadfaedd2d940a3f903d3bb
Tobias Kloht <tobias.kloht@gmail.com>
2015-04-03 17:46:17 +0200

    rework DayNav component

--- src/components/DayNav.jsx
+++ src/components/DayNav.jsx
@@ -1,56 +1,73 @@
+'use strict';
 var React = require('react'),
   Router = require('react-router'),
   mui = require('material-ui'),
-  Tabs = mui.Tabs,
-  Tab = mui.Tab;
+  CourseList = require('./CourseList.jsx');
 
   var DayNav = React.createClass({
     mixins: [Router.Navigation, Router.State],
     render: function() {
-      return (
-        <Tabs>
-          <Tab 
-            label="Monday"
-            route="monday"
-            onActive={this._onActive}
-        />
-        <Tab 
-          label="Tuesday"
-          route="tuesday"
-          onActive={this._onActive}
-        />
-        <Tab 
-          label="Wednesday"
-          route="wednesday"
-          onActive={this._onActive}
-        />
-        <Tab 
-          label="Thursday"
-          route="thursday"
-          onActive={this._onActive}
-        />
-        <Tab 
-          label="Friday"
-          route="friday"
-          onActive={this._onActive}
-        />
-        <Tab 
-          label="Saturday"
-          route="saturday"
-          onActive={this._onActive}
-        />
-        <Tab 
-          label="Sunday"
-          route="sunday"
-          onActive={this._onActive}
-        />
-      </Tabs>
-		);
-	},
 
-  _onActive: function(tab){ 
-    this.transitionTo("/studio/" + this.props.studio + "/day/" + tab.props.route); 
-  }
+      var tabs = this.state.days.map(function(day) {
+        return (
+          <mui.Tab label={day.name} route={day.id} onActive={day.route}>
+            <CourseList/>
+          </mui.Tab>
+          );
+      });
+
+      return (
+        <mui.Tabs>
+          {tabs}
+        </mui.Tabs>
+      );
+    },
+
+    getInitialState: function () {
+      return {
+        days: [
+          {
+            id: 'monday',
+            name: 'Monday',
+            route: this._onActive
+          },
+          {
+            id: 'tuesday',
+            name: 'Tuesday',
+            route: this._onActive
+          },
+          {
+            id: 'wednesday',
+            name: 'Wednesday',
+            route: this._onActive
+          },
+          {
+            id: 'thursday',
+            name: 'Thursday',
+            route: this._onActive
+          },
+          {
+            id: 'friday',
+            name: 'Friday',
+            route: this._onActive
+          },
+          {
+            id: 'saturday',
+            name: 'Saturday',
+            route: this._onActive
+          },
+          {
+            id: 'sunday',
+            name: 'Sunday',
+            route: this._onActive
+          }
+        ]
+      };
+    },
+
+    _onActive: function(tab){
+      this.transitionTo('/studio/' + this.props.studio + '/day/' + tab.props.route);
+    }
 });
 
 module.exports = DayNav;
